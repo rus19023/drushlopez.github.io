a286fded21e16a7a976e73c8a97bc70ce61807a3
rus19023 <rus19023@byui.edu>
2020-03-01 12:58:15 -0500

    fib

---
+++ wk07/fib-geeks.py
@@ -0,0 +1,19 @@
+# Function for nth Fibonacci number 
+  
+def fib(n): 
+    if n<1: 
+        print("Incorrect input") 
+    # First Fibonacci number is 0 
+    elif n==1: 
+        return 0
+    # Second Fibonacci number is 1 
+    elif n==2: 
+        return 1
+    else: 
+        return fib(n-1)+fib(n-2) 
+  
+# Driver Program 
+  
+print(fib(2)) 
+  
+#This code is contributed by Saket Modi 
--- wk07/prove07.py
+++ wk07/prove07.py
@@ -1,15 +1,17 @@
+fibonacci_list = [0, 1.]
+position = 0
+number = 0
 def fib(n):
-    fibonacci_list = []
-    start = 1
-    if number < index:
+        fibonacci_list.append(fibonacci_list([index - 1]) + fibonacci_list([index - 2]))
+        number += 1
         
-     return "The Fibonacci number is: {}".format(number)
+    if number < len(fibonacci_list - 1) and index > 1:
+        return "The Fibonacci number is: {}".format(fibonacci_list[len(fibonacci_list - 1)])
     
     
- 
 def main():
-     index = int(input("Enter a Fibonacci index: "))
-        
+    position = int(input("Enter a Fibonacci index: "))
+    fib(position)
     
     
 if __name__ == "__main__":
